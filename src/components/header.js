9c260701acd0fc205ee85ef6cbcf2d187afe2bb1
Anindya Chowdhury <anindya50@live.com>
2020-02-15 21:45:45 +0530

    Creating the Navbar

--- src/components/header.js
+++ src/components/header.js
@@ -2,44 +2,46 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+
 import './header.css';
 
 const Header = ({ siteTitle }) => (
-	<nav className="navbar navbar-expand-lg navbar-light">
-		<Link className="navbar-brand" href="/">
-			Navbar
+	<nav className="navbar navbar-expand-md navbar-light p-4 text-large">
+		<Link to="/" className="navbar-brand">
+			{siteTitle}
 		</Link>
-		<button
+		<div
 			className="navbar-toggler"
-			type="button"
 			data-toggle="collapse"
-			data-target="#navbarNav"
-			aria-controls="navbarNav"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
+			data-target="#myNav"
 		>
-			<span className="navbar-toggler-icon" />
-		</button>
-		<div className="collapse navbar-collapse" id="navbarNav">
+			<FontAwesomeIcon icon={faHamburger} />
+		</div>
+		<div
+			className="collapse navbar-collapse justify-content-end p-3"
+			id="myNav"
+		>
 			<ul className="navbar-nav">
-				<li className="nav-item active">
-					<Link className="nav-link" href="#">
-						Home <span class="sr-only">(current)</span>
+				<li className="nab-item">
+					<Link to="/" className="nav-link">
+						About
 					</Link>
 				</li>
-				<li className="nav-item">
-					<Link className="nav-link" href="#">
-						Features
+				<li className="nab-item">
+					<Link to="/" className="nav-link">
+						Projects
 					</Link>
 				</li>
-				<li className="nav-item">
-					<Link className="nav-link" href="#">
-						Pricing
+				<li className="nab-item">
+					<Link to="/" className="nav-link">
+						Blog
 					</Link>
 				</li>
-				<li className="nav-item">
-					<Link className="nav-link disabled" href="#">
-						Disabled
+				<li className="nab-item">
+					<Link to="/" className="nav-link">
+						Contact
 					</Link>
 				</li>
 			</ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Link } from 'gatsby';
+// import { Link } from 'gatsby';
 import Header from '../components/header';
 import SEO from '../components/seo';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../styles/index.css';
 
 const IndexPage = () => (
-	<div>
+	<div className="mainBody">
 		<SEO title="Home" />
-		<Header siteTitle="41x3n" />
-		<h1>Hi people</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
+		<div className="container">
+			{' '}
+			<Header siteTitle="41x3n" />
+		</div>
 	</div>
 );
 
 export default IndexPage;
+
+// https://cssgradient.io/gradient-backgrounds/
+// https://codepen.io/jstn/pen/zuDst
