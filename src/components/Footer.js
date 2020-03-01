db92e4fb179a3f16fd7450f455693c83cf87bfb6
Anindya Chowdhury <anindya50@live.com>
2020-03-01 09:55:59 +0530

    Rebooted the design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,23 @@ import React from 'react';
 import footer from './footer.module.css';
 
 const Footer = () => (
-	<div className="container text-light">
-		<div className="row">
-			<div className="col-12 d-md-none">
+	<div className="container">
+		<div className="row navbar fixed-bottom">
+			<div className="col-12">
 				<div className={`${footer.side} d-flex justify-content-center`}>
 					<a href="https://github.com/41x3n">
-						<i class="fab fa-github" />
+						<i class="fab fa-github fa-2x" />
 					</a>
 					<a href="https://www.linkedin.com/in/anindya-41x3n/">
-						<i class="fab fa-linkedin-in" />
+						<i class="fab fa-linkedin-in fa-2x" />
 					</a>
 
 					<a href="https://www.instagram.com/lost_scribe/">
-						<i class="fab fa-instagram" />
+						<i class="fab fa-instagram fa-2x" />
 					</a>
 
 					<a href="https://www.kaggle.com/anindya41x3n">
-						<i class="fab fa-kaggle" />
+						<i class="fab fa-kaggle fa-2x" />
 					</a>
 				</div>
 			</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 // import { Link } from 'gatsby';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Sidebar from '../components/Sidebar';
-import Body from '../components/Body';
 import SEO from '../components/seo';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,14 +15,6 @@ const IndexPage = () => (
 		<SEO title="Home" />
 		<div className="container-fluid">
 			<Header siteTitle="41x3n" />
-			<div className="row index">
-				<div className="col-md-none">
-					<Sidebar />
-				</div>
-				<div className="col-md">
-					<Body />
-				</div>
-			</div>
 			<Footer />
 		</div>
 	</div>
